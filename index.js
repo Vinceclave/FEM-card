d11f5336591d5c6cf9185f47670afa11967bba61
Mr. Beans <93638850+Vinceclave@users.noreply.github.com>
2022-12-15 20:19:56 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const cardHolder = document.getElementById('card-holder')
+const cardNumber = document.getElementById('card-number')
 const securityCode = document.getElementById('security-code')
 
-cardHolder.addEventListener('blur', e => {
+cardHolder.addEventListener('input', e => {
     const holder = e.target.value
     let pattern = /^[a-z ,.'-]+$/i
-    let result = pattern.test(holder)
-    console.log(result)
+    document.querySelector('.name').innerHTML = holder
 })
 
-securityCode.addEventListener('blur', e => {
+securityCode.addEventListener('input', e => {
     const cvc = e.target.value
     let pattern = /\b\d{1,3}\b/
-    let result = pattern.test(cvc)
-    console.log(result)
+    document.querySelector('.code').innerHTML = cvc
+})
+
+cardNumber.addEventListener('input', e => {
+    const card = e.target.value
+    let div = card.split(' ').join(' ')    
+    if(card.length > 0) {
+        div = div.match(new RegExp('.{1,4}', 'g')).join(' ')
+    }
+    document.querySelector('.card').innerHTML = div
 })
